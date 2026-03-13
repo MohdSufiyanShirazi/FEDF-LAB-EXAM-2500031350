2b8da94ff3fc0233369edead35b7182fdf220bc3
Sufiyan <2500031350@kluniversity.in>
2026-03-13 12:25:27 +0530

    modified

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div>
         <h2>Welcome to the Home Page <br/>
-        Please click on the Tabs above to navigate through the website.
+        Please click on Book Registration to Register a Book <br/>
+        Please click on View Books to see the Registered Books <br/>
+        Please click on Api Demo to see the API Demo <br/>
         </h2>
     </div>
   )
