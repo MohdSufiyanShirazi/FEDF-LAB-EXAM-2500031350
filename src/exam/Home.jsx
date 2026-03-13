0f71b8cd158267fb2d91d7a6f79d39f5a16519b1
Sufiyan <2500031350@kluniversity.in>
2026-03-13 10:09:51 +0530

    project v1.0

---
+++ src/exam/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2>Welcome to the Home Page <br/>
+        Please click on the Tabs above to navigate through the website.
+        </h2>
+    </div>
+  )
+}
